a164f544b7a4375839645d0bbd656638581e8842
ANIKET INAMDAR <aniketinamdar02@gmail.com>
2024-02-08 18:48:18 +0530

    Data Transformation updated

--- src/MLOps_Test/pipeline/stage_03_data_transformation.py
+++ src/MLOps_Test/pipeline/stage_03_data_transformation.py
@@ -1,6 +1,7 @@
 from MLOps_Test.config.configuration import ConfigurationManager
 from MLOps_Test.components.data_transformation import DataTransformation
 from MLOps_Test import logger
+from pathlib import Path
 
 stage_name = "Data Transformation Stage"
 
@@ -9,10 +10,19 @@ class DataTransformationTrainingPipeline:
         pass
 
     def main(self):
-        config = ConfigurationManager()
-        data_transformation_config = config.get_data_transformation_config()
-        data_transformation = DataTransformation(config=data_transformation_config)
-        data_transformation.train_test_splitting()
+        try:
+            with open(Path("artifacts/data_validation/status.txt"), "r") as f:
+                status = f.read().split(" ")[-1]
+
+            if status == "True":
+                config = ConfigurationManager()
+                data_transformation_config = config.get_data_transformation_config()
+                data_transformation = DataTransformation(config=data_transformation_config)
+                data_transformation.train_test_splitting()
+            else:
+                raise Exception("Data Schema is not valid")
+        except Exception as e:
+            print(e)
 
 if __name__ == "__main__":
     try:
